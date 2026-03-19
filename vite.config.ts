016ca50c413e54c86469a56085c5a306a79874c2
Berat Sulimani <berat_01993@hotmail.com>
2026-03-19 21:48:20 +0100

    fi

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,12 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vite"
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt'],
-      manifest: false
-    })
+    tailwindcss()
   ],
   resolve: {
     alias: {
